13fac67b79be55c8ee971152065ec8e5dc51859a
damodars <damodars@sci.utah.edu>
2020-09-15 19:09:01 -0600

    Reintroduced "adaptive" part in the hierarchical parallelism. Bug Fixed.
    Bug: Only 3 out 4 hypre__i values were copied in boxloop macro functors causing crash and unexpeted behaviour. Fixed.

--- hypre_ep/src/utilities/hypre_mpi_ep_helper.h
+++ hypre_ep/src/utilities/hypre_mpi_ep_helper.h
@@ -8,7 +8,7 @@
 #include <atomic>
 
 #define USE_INTER_THREAD_COMM
-#define USE_FUNNELLED_COMM
+//#define USE_FUNNELLED_COMM
 /*do not define USE_FUNNELLED_COMM here. use -DUSE_FUNNELLED_COMM compiler option instead*/
 #define USE_MULTIPLE_COMMS
 #define USE_ODD_EVEN_COMMS
